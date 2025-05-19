9aaa4bf677c1a959e2d2eaf19d2f98a11e0d4a34
Zn1chk4 <s30267@pjwstk.edu.pl>
2025-05-19 15:30:29 +0200

    19.05
    asd

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float speed = 5;
+    public float moveSpeed = 5;
+    public float runSpeed = 7;
+    public float jumpForce = 300;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
     }
 
 
     void Update()
     {
         float moveInput = Input.GetAxis("Horizontal");
-        // Debug.Log ($"Input value: {moveInput}");
-        rb.velocity = new Vector2(moveInput + speed, rb.velocity.y);
 
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rb.velocity = new Vector2(moveInput * runSpeed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+        }
     }
 }
